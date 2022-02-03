d424c903756afac94a9cb55d29681f969b7b7843
William <william.9150@gmail.com>
2022-02-03 11:39:26 +0800

    add folder ViewModel and Factory. add PreOrderInfoViewModel. add CampOrderController

---
+++ CampWebsite/CampWebsite/Controllers/CampOrderController.cs
@@ -0,0 +1,29 @@
+﻿using CampWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampWebsite.Controllers
+{
+    public class CampOrderController : Controller
+    {
+        dbCampEntities db = new dbCampEntities();
+        // GET: CampOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult GenerateOrder(int TentID)
+        {
+            tOrder newOrder = new tOrder();
+            DateTime CheckinDate = DateTime.Today;
+            newOrder.fTentID = TentID;
+            newOrder.fMemberID = Convert.ToInt32(User.Identity.Name);
+            newOrder.fCheckinDate = CheckinDate;
+            return View(newOrder);
+        }
+    }
+}
---
+++ CampWebsite/CampWebsite/Models/ViewModel/PreOrderInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampWebsite.Models.ViewModel
+{
+    public class PreOrderInfoViewModel
+    {
+        public tMember tMember { get; set; }
+        public tTent tTent { get; set; }
+        public tOrder tOrder { get; set; }
+    }
+}
